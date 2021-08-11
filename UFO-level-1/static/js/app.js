a313347a3df74d8f8c56de7474bd8945e18c5cc0
alan.seed <alan.seed@gmail.com>
2021-08-11 12:32:23 +1000

    select data working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button and form 
+var button = d3.select("#filter-btn");
+button.on("click", runEnter); 
+
+var form = d3.select("#datetime");
+// TO DO - Not sure why the submit is not working 
+
+form.on("select", runEnter)
+
+function runEnter(){ 
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    // filter the data by this date 
+    var filteredData = data.filter(data => data.datetime === inputValue);
+    console.log(filteredData);
+  
+}
+
