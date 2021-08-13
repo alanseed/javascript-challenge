c99c23347ed59826f54a1f339b7749f87361ef8d
alan.seed <alan.seed@gmail.com>
2021-08-13 12:08:25 +1000

    initial commit for level-2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,41 @@
+// get the luxon date object 
+var DateTime = luxon.DateTime; 
+
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button and form 
+var button = d3.select("#filter-btn");
+button.on("click", runEnter); 
+
+var form = d3.select("#form"); 
+form.on("submit",runEnter);
+
+var tbody = d3.select("tbody");
+
+function runEnter() {
+  d3.event.preventDefault();
+
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  var tDate = DateTime.fromFormat(inputValue, "d/M/yyyy")
+  
+  // remove the existing rows in the table
+  tbody.selectAll("tr").remove(); 
+
+  // filter the data by this date if it is valid
+  if (tDate.isValid) {
+    var filteredData = data.filter(data => data.datetime === inputValue);
+    
+    filteredData.forEach((ufoReport) => {
+      var row = tbody.append("tr");
+      Object.entries(ufoReport).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    })
+  }
+  else {
+    document.getElementById("datetime").value = "1/11/2011"
+  }
+}
