6eeb840a766f49b85455809f89efe37143fd92c6
alanseed <alan.seed@gmail.com>
2021-08-17 11:23:26 +1000

    WIP list of states working

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,5 +1,5 @@
 // get the luxon date object 
-
+var DateTime = luxon.DateTime; 
 
 // from data.js
 var tableData = data;
@@ -9,23 +9,91 @@ var button = d3.select("#sumbitButton");
 button.on("click", runEnter); 
 
 var tbody = d3.select("tbody");
+var startDate = getStartDate(data);
+document.getElementById("startDateSelector").value = startDate.toISODate();
+
+var endDate = getEndDate(data);
+document.getElementById("endDateSelector").value = endDate.toISODate();
+var stateList = getStateList(data);
+
+
 
 function runEnter() {
   d3.event.preventDefault();
-  var startDate = document.getElementById("startDateSelector").value;
-  var endDate = document.getElementById("endDateSelector").value;
 
+  let start = document.getElementById("startDateSelector").value;
+  let end = document.getElementById("endDateSelector").value;
+  startDate = DateTime.fromISO(start);
+  endDate = DateTime.fromISO(end);
+  
   // remove the existing rows in the table
   tbody.selectAll("tr").remove(); 
 
   // filter the data by these dates
-  var filteredData = data.filter(data => data.datetime === startDate);
+  var filteredData = data.filter(filterByDate);
     
   filteredData.forEach((ufoReport) => {
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+      if (key != "country"){
+        var cell = row.append("td");
+        if ( key === 'datetime'){
+          let reportDate = DateTime.fromFormat(value, "M/d/yyyy") ;
+          value = reportDate.toISODate();
+        }
+        cell.text(value);
+      }
     });
   })
+}
+
+function filterByDate(report){
+  var reportDate = DateTime.fromFormat(report.datetime, "M/d/yyyy") ; 
+  if ( reportDate.toSeconds() >= startDate.toSeconds() && 
+      reportDate.toSeconds() <= endDate.toSeconds() ){
+        return true ;
+      } 
+  else {
+     return false; 
+  }
+}
+
+function getStartDate(data){
+  let minDate = DateTime.utc() ;
+  let reportDate ;
+  for ( let i = 0; i < data.length; i++ ){
+    reportDate = DateTime.fromFormat(data[i].datetime, "M/d/yyyy") ;
+    if ( reportDate.toSeconds() < minDate.toSeconds() ){ 
+      minDate = reportDate;
+    }
+  }
+  return minDate ; 
+}
+
+function getEndDate(data){
+  let maxDate = DateTime.fromISO("1970-01-01T00:00:00") ;
+  let reportDate ;
+  for ( let i = 0; i < data.length; i++ ){
+    reportDate = DateTime.fromFormat(data[i].datetime, "M/d/yyyy") ;
+    if ( reportDate.toSeconds() > maxDate.toSeconds() ){ 
+      maxDate = reportDate;
+    }
+  }
+  return maxDate ; 
+} 
+
+function getStateList(data){
+  var states = [];
+  for ( let i = 0; i < data.length; i++){ 
+    addState(states, data[i].state); 
+  }
+  return states ;
+}
+
+function addState(states, state){ 
+  for ( let i = 0; i < states.length; i++){
+    if (state == states[i] ) return ; 
+  }
+  states.push(state);
+  return ; 
 }
